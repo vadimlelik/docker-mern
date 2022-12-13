6a4631ae5336c7d8b9075d3b1d526054c73eb754
vadimcvirko <promiti90@gmail.com>
2022-12-13 20:26:46 +0300

    docker server

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
-import {useCallback, useEffect, useState} from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import axios from 'axios'
-import {List} from './List'
-import {Form} from './Form'
+import { List } from './List'
+import { Form } from './Form'
+
+
+const baseURL = 'http://localhost:5000'
+
+const api = axios.create({
+  baseURL
+})
+
 
 function App() {
   const [notes, setNotes] = useState([])
 
   async function createNote(text) {
-    const note = await axios.post('/api/note', {text})
-    setNotes([...notes, {...note.data.note}])
+    const note = await api.post('/api/note', { text })
+    setNotes([...notes, { ...note.data.note }])
   }
 
   const fetchNotes = useCallback(async () => {
-    const notes = await axios.get('/api/note')
+    const notes = await api.get('/api/note')
     setNotes(notes.data)
   }, [])
 
