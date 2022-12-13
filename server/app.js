bcf6a328701c0c00fade8546940f1a837ed5bb85
vadimcvirko <promiti90@gmail.com>
2022-12-13 20:25:09 +0300

    docker server

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const routes = require('./routes/note.routes')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT ?? 5000
 
+app.use(cors())
 app.use(express.json({ extended: true }))
 app.use('/api/note', routes)
 
@@ -19,10 +21,11 @@ if (process.env.NODE_ENV === 'production') {
 
 async function start() {
   try {
-    await mongoose.connect(`mongodb://localhost:27017/notes`, {
+    await mongoose.connect(`mongodb://host.docker.internal:27017/notes`, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
+    console.log('mongoDb connected');
     app.listen(
       port,
       console.log.bind(console, `Server has been started on port ${port}`)
